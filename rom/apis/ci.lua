336a35317f227862e87a0d0a500b49830c67c21c
walksanatora <walkerffo22@gmail.com>
2023-01-13 09:47:10 -0800

    make a folder

--- rom/apis/ci.lua
+++ rom/apis/ci.lua
@@ -3,8 +3,10 @@ status = {}
 status.ok = 0
 status.error = 1
 
+fs.makeDir("/.ci")
+
 function exit(status, message)
-    local ci_file = fs.open("/.CCCI","wb")
+    local ci_file = fs.open("/.ci/status","wb")
     ci_file.write(tostring(status))
     ci_file.write("\t")
     ci_file.write(message or "")
