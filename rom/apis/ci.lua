fd7957084ce733530e23dae813a756ce033b4e69
walksanatora <walkerffo22@gmail.com>
2023-01-13 09:55:21 -0800

    just allow the CI to print exit status

--- rom/apis/ci.lua
+++ rom/apis/ci.lua
@@ -1,15 +1,9 @@
 --CI library for CraftosPC CI situations
-status = {}
-status.ok = 0
-status.error = 1
-
 fs.makeDir("/.ci")
 
-function exit(status, message)
+function exit(status)
     local ci_file = fs.open("/.ci/status","wb")
     ci_file.write(tostring(status))
-    ci_file.write("\t")
-    ci_file.write(message or "")
     ci_file.close()
     os.shutdown()
 end
